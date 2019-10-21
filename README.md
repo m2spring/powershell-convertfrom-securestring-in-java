b4d42ca1dfdd23918cfdb2f99df92a49564772cc
mspring <mspring@cisco.com>
2019-10-21 16:31:57 -0700

    added PowerShell example

---
+++ README.md
@@ -0,0 +1,1 @@
+Code snippets for [PowerShell ConvertFrom-SecureString in Java?](https://stackoverflow.com/questions/58423051/powershell-convertfrom-securestring-in-java) post on stackoverlow.
---
+++ encrypt.ps1
@@ -0,0 +1,17 @@
+$key = 202,144,73,88,228,1,7,104,95,212,137,87,125,201,80,75,53,18,39,108,60,218,212,151,239,241,34,117,106,184,212,179
+
+$secret = "banana"
+Write-Output "secret   : $($secret)"
+
+# encrypt
+$secretSecured = ConvertTo-SecureString $secret -AsPlainText -Force
+$encrypted = ConvertFrom-SecureString -secureString $secretSecured -key $key
+Write-Output "encrypted: $($encrypted)"
+
+$encrypted = "QdcElEmsZJltV5vcCs0vA=="
+
+# decrypt
+$secure = $encrypted | ConvertTo-SecureString -key $key
+$bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secure)
+$decrypted = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+Write-Output "decrypted: $($decrypted)"
